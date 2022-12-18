27f5618e595e52c6b8f258f5ec7f305d761d713a
hilalevi4321@gmail.com <hilalevi4321@gmail.com>
2022-12-18 13:10:04 +0000

    developing stg_orders

---
+++ models/staging/stg_orders.sql
@@ -0,0 +1,18 @@
+select o.orderid,
+       o.orderdate,
+       o.SHPIDATE,
+       o.shipmode,
+       o.ORDERSELLINGPRICE-o.ORDERCOSTPRICE AS orders_profit,
+       o.ORDERSELLINGPRICE,
+       o.ORDERCOSTPRICE,
+       c.customername,
+       c.segment,
+       c.country,
+       g.category,
+       g.productname,
+       g.subcategory
+from {{ ref('raw_orders') }} as o
+left join {{ ref('raw_customers') }} as c
+on o.customerid=c.customerid
+left join {{ ref('raw_products') }} as g
+on o.productid=g.productid
