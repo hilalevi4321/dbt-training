8e3ce0cef3e7704e678f169bde1780f067900967
hilalevi4321@gmail.com <hilalevi4321@gmail.com>
2022-12-26 10:15:42 +0000

    create dim_orders

---
+++ snapshots/order_snapshot.sql
@@ -0,0 +1,17 @@
+{% snapshot orders_snapshot %}
+
+{{
+    config(
+        target_database="analytics",
+        target_schema="snapshots",
+        unique_key="order_id",
+        strategy="timestamp",
+        updated_at="inserted_at",
+    )
+}} select inserted_at,TYPE,
+            ORDER_AMOUNT,
+            ORDER_STATUS,
+            ORDER_ID,
+            ORDER_TYPE from {{ ref("stg_order") }}
+
+{% endsnapshot %}
